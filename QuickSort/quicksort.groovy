55e6ac80187224ca2bc1abfa26d693a1f497c1a7
yamap55 <tetuya120@hotmail.com>
2016-01-17 00:22:43 +0900

    fixed #1 とりあえず実装。

--- QuickSort/quicksort.groovy
+++ QuickSort/quicksort.groovy
@@ -7,9 +7,17 @@ def createRandomList = {size ->
 def randomList = createRandomList(10)
 println randomList
 
-def quickSort = {
-  it
+def quickSort = {result, list ->
+  println "result : ${result}, list : ${list}"
+  if (list.size() <= 1 || list.every{list[0] == it}) {
+    result.addAll(list)
+    return result
+  }
+  def pivot = list.pop()
+  def small = list.findAll{it < pivot}
+  def big = list.findAll{it >= pivot}
+  big.add(0,pivot)
+  call(result, small)
+  call(result,big)
 }
-
-
-println quickSort(randomList)
+println quickSort([],randomList)
